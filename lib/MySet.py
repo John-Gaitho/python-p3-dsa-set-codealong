c9248ab7355ef4ca38c737efff26ae8064477e7a
John Gaitho <jgaitho016@gmail.com>
2025-01-03 15:50:45 +0300

    complete code

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,27 @@
 class MySet:
+    def __init__(self, elements=None):
+        self.dictionary = {}
+        if elements:
+            for element in elements:
+                self.dictionary[element] = True
 
-    pass
+    def add(self, element):
+        self.dictionary[element] = True
+
+    def delete(self, element):
+        if element in self.dictionary:
+            del self.dictionary[element]
+
+    def has(self, element):
+        return element in self.dictionary
+
+    def size(self):
+        return len(self.dictionary)
+
+    def clear(self):
+        self.dictionary.clear()
+
+
+    def __str__(self):
+        elements = ', '.join(map(str, self.dictionary.keys()))
+        return f"MySet: {{{elements}}}"
